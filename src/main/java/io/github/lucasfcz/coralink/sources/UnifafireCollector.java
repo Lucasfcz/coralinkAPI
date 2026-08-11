bb3e62ea7c0af7802d35505f15edc2754c98746a
Lucas <lucasfc080@gmail.com>
2026-08-10 22:05:30 -0300

    feature: implemented UNIBRA and UNIFAFIRE collectors and HTML and WordPress debugger

---
+++ src/main/java/io/github/lucasfcz/coralink/sources/UnifafireCollector.java
@@ -0,0 +1,31 @@
+package io.github.lucasfcz.coralink.sources;
+
+import io.github.lucasfcz.coralink.dto.DetailedContent;
+import io.github.lucasfcz.coralink.enums.SourceName;
+import io.github.lucasfcz.coralink.exceptions.CollectException;
+import io.github.lucasfcz.coralink.sources.collector.WordPressCollector;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnifafireCollector extends WordPressCollector {
+
+    private static final String BASE_URL = "https://unifafire.edu.br";
+
+    @Override
+    protected String baseUrl() {
+        return BASE_URL;
+    }
+
+    @Override
+    protected String imageFallBackUrl() {
+        return "https://unifafire.edu.br/wp-content/uploads/2022/04/cropped-180x180_frassino-2-192x192.png";
+    }
+
+
+    @Override
+    public SourceName sourceName() {
+        return SourceName.UNIFAFIRE;
+    }
+}
